d1c3c8eaf7f2e15fe0a16b0905abad774d7fbd42
leandre <tuyambazeleandre@gmail.com>
2026-04-27 19:00:00 +0200

    ft(spring-security): add security package and implement security configuration

---
+++ src/main/java/com/ally/blogapp/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.ally.blogapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement(c ->
+                        c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(c -> c.disable())
+                .authorizeHttpRequests( c -> c
+                        .requestMatchers("/api/auth").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
